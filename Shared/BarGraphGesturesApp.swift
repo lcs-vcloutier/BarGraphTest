7bb5aad429ca7e5068d3ae8db4871c87763588ff
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-11-22 17:44:22 -0500

    Initial Commit

---
+++ Shared/BarGraphGesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarGraphGesturesApp.swift
+//  Shared
+//
+//  Created by Vincent Cloutier on 2021-11-22.
+//
+
+import SwiftUI
+
+@main
+struct BarGraphGesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
