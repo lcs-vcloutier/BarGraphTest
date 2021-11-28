16aee6b4594b70583ff3ba80571819e4824ef1fb
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-11-28 15:54:32 -0500

    started gesture work and added cardview code

--- Shared/View/BarGraph.swift
+++ Shared/View/BarGraph.swift
@@ -9,25 +9,41 @@ import SwiftUI
 
 struct BarGraph: View {
     var downloads: [Download]
+    
+    // Gesture Properties...
+    @GestureState var isDragging: Bool = false
+    @State var offset: CGFloat = 0
+    
+    //Current download to highlight
+    @State var currentDownloadId: String = ""
+    
     var body: some View {
 
         HStack(spacing: 10) {
             ForEach(downloads) { download in
-            
-                VStack(spacing: 20) {
-                    GeometryReader{ proxy in
-                        let size = proxy.size
-                        RoundedRectangle(cornerRadius: 6)
-                            .fill(download.color)
-                            .frame(height: (download.downloads / getMax()) * (size.height))
-                            .frame(maxHeight: .infinity, alignment: .bottom)
-                    }
-                }
+            CardView(download: download)
             }
-            
         }
         .frame(height: 150)
     }
+    
+    @ViewBuilder
+    func CardView(download: Download) -> some View {
+        VStack(spacing: 20) {
+            GeometryReader{ proxy in
+                let size = proxy.size
+                RoundedRectangle(cornerRadius: 6)
+                    .fill(download.color)
+                    .opacity(isDragging ? (currentDownloadId == download.id ? 1 : 0.35) : 1)
+                    .frame(height: (download.downloads / getMax()) * (size.height))
+                    .frame(maxHeight: .infinity, alignment: .bottom)
+            }
+            Text(download.day)
+                .font(.callout)
+                .foregroundColor(currentDownloadId == download.id ? download.color : .gray)
+        }
+    }
+     
     // Get each download's height
     func getMax() -> CGFloat {
         let max = downloads.max { first, second in
